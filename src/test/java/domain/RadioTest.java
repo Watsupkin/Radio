bc5a277a65444172d7a7fa86f27be88c959247d6
Andrey Spiridonov <andrejvazapkin@gmail.com>
2022-03-09 16:14:31 +0400

    Radio2

--- src/test/java/domain/RadioTest.java
+++ src/test/java/domain/RadioTest.java
@@ -10,7 +10,7 @@ class RadioTest {
 
 
     @ParameterizedTest
-    @CsvSource(value ={"lowLimit,0,0","underLowLimit,-1,9","aboveLowLimit,1,1","upperLimit,9,9","underUpperLimit,8,8","aboveUpperLimit,10,0"})
+    @CsvSource(value ={"lowLimit,0,0","underLowLimit,-1,10","aboveLowLimit,1,1","upperLimit,10,10","underUpperLimit,9,9","aboveUpperLimit,11,0"})
     void shouldSetStation(String testName, int newCurrentStation, int expected) {
         Radio pioneer = new Radio();
         int actual = pioneer.setStation(newCurrentStation);
@@ -19,7 +19,7 @@ class RadioTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value ={"limitValue,9,0",/*"aboveLimitValue,10,0",*/"underLimitValue,8,9"})
+    @CsvSource(value ={"limitValue,10,0",/*"aboveLimitValue,10,0",*/"underLimitValue,9,10"})
     void shouldNext(String testName, int newCurrentStation, int expected) {
         Radio pioneer = new Radio();
         pioneer.setStation(newCurrentStation);
@@ -28,7 +28,7 @@ class RadioTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value ={"limitValue,0,9","aboveLimitValue,1,0",/*"underLimitValue,-1,9"*/})
+    @CsvSource(value ={"limitValue,0,10","aboveLimitValue,1,0",/*"underLimitValue,-1,9"*/})
     void shouldPrev(String testName, int newCurrentStation, int expected) {
         Radio pioneer = new Radio();
         pioneer.setStation(newCurrentStation);
@@ -37,8 +37,8 @@ class RadioTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value ={"lowLimit,0,0","underLowLimit,-1,0","aboveLowLimit,1,1","upperLimit,10,10","underUpperLimit,9,9","aboveUpperLimit,11,10"})
-    void sholdSetVolume(String testName, int newVolume, int expected) {
+    @CsvSource(value ={"lowLimit,0,0","underLowLimit,-1,0","aboveLowLimit,1,1","upperLimit,100,100","underUpperLimit,99,99","aboveUpperLimit,101,100"})
+    void shouldSetVolume(String testName, int newVolume, int expected) {
         Radio pioneer = new Radio();
         int actual = pioneer.setVolume (newVolume);
         assertEquals(expected, actual);
@@ -47,7 +47,7 @@ class RadioTest {
     void plusVolume() {
     }
     @ParameterizedTest
-    @CsvSource(value ={"limitValue,10,10","aboveLimitValue,11,10","underLimitValue,8,9"})
+    @CsvSource(value ={"limitValue,100,100","aboveLimitValue,101,100","underLimitValue,90,91"})
     void shouldPlusVolume(String testName, int newVolume, int expected) {
         Radio pioneer = new Radio();
         pioneer.setVolume(newVolume);
