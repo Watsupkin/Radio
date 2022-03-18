e61aa59e7c630987e9aeabfa9403e7cced8ed5c8
Andrey Spiridonov <andrejvazapkin@gmail.com>
2022-03-18 05:14:15 +0400

    Constructors

--- src/main/java/domain/Radio.java
+++ src/main/java/domain/Radio.java
@@ -1,5 +1,10 @@
 package domain;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
 public class Radio {
     private int currentStation;
     private int volume;
--- src/test/java/domain/RadioTest.java
+++ src/test/java/domain/RadioTest.java
@@ -7,12 +7,13 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
+    Radio pioneer = new Radio();
 
 
     @ParameterizedTest
     @CsvSource(value ={"lowLimit,0,0","underLowLimit,-1,10","aboveLowLimit,1,1","upperLimit,10,10","underUpperLimit,9,9","aboveUpperLimit,11,0"})
     void shouldSetStation(String testName, int newCurrentStation, int expected) {
-        Radio pioneer = new Radio();
+
         int actual = pioneer.setStation(newCurrentStation);
         assertEquals(expected, actual);
 
@@ -21,7 +22,7 @@ class RadioTest {
     @ParameterizedTest
     @CsvSource(value ={"limitValue,10,0",/*"aboveLimitValue,10,0",*/"underLimitValue,9,10"})
     void shouldNext(String testName, int newCurrentStation, int expected) {
-        Radio pioneer = new Radio();
+
         pioneer.setStation(newCurrentStation);
         int actual = pioneer.next();
         assertEquals(expected, actual);
@@ -30,7 +31,7 @@ class RadioTest {
     @ParameterizedTest
     @CsvSource(value ={"limitValue,0,10","aboveLimitValue,1,0",/*"underLimitValue,-1,9"*/})
     void shouldPrev(String testName, int newCurrentStation, int expected) {
-        Radio pioneer = new Radio();
+
         pioneer.setStation(newCurrentStation);
         int actual = pioneer.prev();
         assertEquals(expected, actual);
@@ -39,7 +40,7 @@ class RadioTest {
     @ParameterizedTest
     @CsvSource(value ={"lowLimit,0,0","underLowLimit,-1,0","aboveLowLimit,1,1","upperLimit,100,100","underUpperLimit,99,99","aboveUpperLimit,101,100"})
     void shouldSetVolume(String testName, int newVolume, int expected) {
-        Radio pioneer = new Radio();
+
         int actual = pioneer.setVolume (newVolume);
         assertEquals(expected, actual);
     }
@@ -49,7 +50,7 @@ class RadioTest {
     @ParameterizedTest
     @CsvSource(value ={"limitValue,100,100","aboveLimitValue,101,100","underLimitValue,90,91"})
     void shouldPlusVolume(String testName, int newVolume, int expected) {
-        Radio pioneer = new Radio();
+
         pioneer.setVolume(newVolume);
         int actual = pioneer.plusVolume();
         assertEquals(expected, actual);
@@ -58,7 +59,7 @@ class RadioTest {
     @ParameterizedTest
     @CsvSource(value ={"limitValue,0,0","aboveLimitValue,2,1","underLimitValue,-1,0"})
     void shouldMinusVolume(String testName, int newVolume, int expected) {
-        Radio pioneer = new Radio();
+
         pioneer.setVolume(newVolume);
         int actual = pioneer.minusVolume();
         assertEquals(expected, actual);
